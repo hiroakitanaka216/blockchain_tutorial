d791fc91ae7d8fd9f944bd5ce6efc4db3b808509
hiroakitanaka <hiroaki-tanaka216@keio.jp>
2021-05-26 13:05:43 +0900

    about mining(PoW)

--- blockchain.py
+++ blockchain.py
@@ -3,6 +3,7 @@ import hashlib
 import json
 import datetime
 import csv
+import time
 
 class Block:
     def __init__(self, index, timestamp, transaction, previous_hash):
@@ -10,8 +11,9 @@ class Block:
         self.timestamp = timestamp
         self.transaction = transaction
         self.previous_hash = previous_hash
-        self.difficulty = 4  # 難易度を追加, ハッシュの頭n(=4)桁が全部0
+        self.difficulty = 5  # 難易度を追加, ハッシュの頭n(=4)桁が全部0
         self.property_dict = {str(i): j for i, j in self.__dict__.items()}
+        # ハッシュ計算に必要な要素を辞書化
         self.now_hash = self.calc_hash()
         self.proof = None # プルーフを追加
         self.proof_hash = None  # プルーフを追加して計算したハッシュ
@@ -22,6 +24,7 @@ class Block:
         # 少しでもファイルのデータが異なると全く違う値になる
         # ハッシュは簡単に生成できるが、ハッシュから元のファイルを復元するのはすごく大変
         block_string = json.dumps(self.property_dict, sort_keys=True).encode('ascii')
+        # ハッシュ計算関数に代入するための形を整える用にjson
         return hashlib.sha256(block_string).hexdigest()
  
     # プルーフの検証用関数
@@ -58,6 +61,7 @@ def new_transaction(sender, recipient, amount):
 print("please input the number of the new blocks")
 num = int(input())
 
+t1 = time.time()
 block_chain = []
  
 block = Block(0, 0, [], '-') #最初のブロックを作成
@@ -65,9 +69,11 @@ block = Block(0, 0, [], '-') #最初のブロックを作成
 block.proof = block.mining()
  
 block_chain.append(block)
+
+f = open('blockchain_info.csv', 'a')
  
 for i in range(num):
-    block = Block(i+1, str(datetime.datetime.now()), ["適当なトランザクション"], block_chain[i].now_hash)
+    block = Block(i+1, str(datetime.datetime.now()), ["とあるトランザクション"], block_chain[i].now_hash)
     block.proof = block.mining()
     block_chain.append(block)
  
@@ -76,7 +82,12 @@ for block in block_chain:
         print(key, ':', value)
     print("")
 
-f = open('blockchain_info.csv', 'a')
 for key, value in block.__dict__.items():
     f.write(f'{key}:{value}\n')
-    f.write("")
+
+t2 = time.time()
+elapse_time = t2-t1
+print(f"経過時間:{elapse_time}[s]")
+f.write(f"経過時間:{elapse_time}[s]")
+print(f'difficulty:{block.difficulty}')
+f.write(f'difficulty:{block.difficulty}')
